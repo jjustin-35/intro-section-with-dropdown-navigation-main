1808a9680655c037a5e51f5d1fca2a5e9795c769
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-05-10 00:06:09 +0800

    v1.06: complete the function of desktop version

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let features = document.querySelector()
+let features = document.querySelector('#feature');
+let company = document.querySelector('#company');
+let subListElements = [features, company];
+
+subListElements.forEach(element => {
+    element.addEventListener('click', () => {
+        let dropdown = element.children[1];
+        dropdown.classList.toggle('show');
+    })
+})
